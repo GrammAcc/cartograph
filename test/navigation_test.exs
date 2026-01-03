0bbad01b4af4bad8227987d089fb7623c5f63b2c
Dalton Lang <35282898+GrammAcc@users.noreply.github.com>
2026-01-03 15:21:33 -0600

    fix: js push event options passthrough (#4)
    The documentation for `cartograph_patch/1` and `cartograph_navigate/2`
    said that any options other than `:query` and `:value` were passed
    through to the `JS.push/2` call unchanged, but this was not implemented,
    so passthrough of additional options did not ocurr.
    
    Additionally, it doesn't make sense for all of the options to
    `JS.push/2` to be used in the context of the cartograph events.
    Specifically, the target option should not be used since cartograph
    provides an event handler on the LiveView for these navigation events.
    
    This change fixes the bug so that options will be passed through, but it
    also explicitly passes through only supported options. Currently, this
    is `:loading` and `:page_loading`.
    
    Also refactored the query parsing test cases to use parametrized tests
    instead of duplicating the test case code for every variant.

--- test/navigation_test.exs
+++ test/navigation_test.exs
@@ -13,6 +13,23 @@ defmodule Test.Navigation do
     {:remove, "key-only list", [:foo]},
   ]
 
+  base_urls = [
+    "/",
+    "/page/",
+    "/page?foo=bar",
+    "/page/?foo=bar",
+    "https://localhost:4000/",
+    "https://localhost:4000/page",
+    "https://localhost:4000/page/",
+    "https://localhost:4000/page?foo=bar",
+    "https://localhost:4000/page/?foo=bar",
+  ]
+
+  passthrough_opts = [
+    :loading,
+    :page_loading,
+  ]
+
   describe "cartograph navigation events" do
     for {op, desc, val} <- cases do
       test "patch: #{op} - #{desc}" do
@@ -26,15 +43,34 @@ defmodule Test.Navigation do
         assert_does_not_raise(Cartograph.Component.cartograph_patch(query_opts))
       end
 
-      test "navigate: #{op} - #{desc}" do
-        op = unquote(op)
-        val = unquote(Macro.escape(val))
+      for url <- base_urls do
+        test "navigate: #{op} - #{desc} - #{url}" do
+          op = unquote(op)
+          val = unquote(Macro.escape(val))
+          url = unquote(url)
 
-        query_opts = [
-          query: [{op, val}],
-        ]
+          query_opts = [
+            query: [{op, val}],
+          ]
 
-        assert_does_not_raise(Cartograph.Component.cartograph_navigate("/", query_opts))
+          assert_does_not_raise(Cartograph.Component.cartograph_navigate(url, query_opts))
+        end
+      end
+    end
+
+    for opt <- passthrough_opts do
+      test "cartograph_patch passes #{opt} through to JS.push/2" do
+        opt = unquote(opt)
+        opts = [{opt, "value"}]
+        sut = Cartograph.Component.cartograph_patch(opts)
+        assert inspect(sut) =~ " #{to_string(opt)}: "
+      end
+
+      test "cartograph_navigate passes #{opt} through to JS.push/2" do
+        opt = unquote(opt)
+        opts = [{opt, "value"}]
+        sut = Cartograph.Component.cartograph_navigate("/", opts)
+        assert inspect(sut) =~ " #{to_string(opt)}: "
       end
     end
   end
